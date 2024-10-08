eec5c390e763e73049f2e5630dccb3c7e59df0d5
cthiounn <45511529+cthiounn@users.noreply.github.com>
2024-10-08 14:22:04 +0200

    Use case classif llm : notebook (#8)
    * notebooks use case classif
    
    * update notebook
    
    * parse llama31 results for sklearn
    
    * add init script
    
    * update++
    
    * classification
    
    * fix bad index
    
    * update branch use_case_classif_llm (#9)
    
    * Use case classif llm (#1)
    
    * notebooks use case classif
    
    * update notebook
    
    * parse llama31 results for sklearn
    
    * add init script
    
    * update++
    
    * classification
    
    * fix bad index
    
    * Update 2_Classification_accords_entreprise.qmd
    
    * Update 2_Classification_accords_entreprise.qmd
    
    * Update main.yaml
    
    * Update main.yaml
    
    * Update 2_Classification_accords_entreprise.qmd
    
    * Update 2_Classification_accords_entreprise.qmd
    
    * freeze version
    
    * Delete notebooks/autres/.ipynb_checkpoints/parse_llama31_results-checkpoint.ipynb
    
    * Update requirements.txt
    
    * Update 2_Classification_accords_entreprise.qmd

---
+++ notebooks/init-env-download.sh
@@ -0,0 +1,4 @@
+sudo apt update >/dev/null 2>/dev/null && sudo apt install --yes lshw >/dev/null 2>/dev/null
+curl -fsSL https://ollama.com/install.sh | sh
+pip install -r  requirements.txt
+curl https://minio.lab.sspcloud.fr/cthiounn2/Accords/10p_accords_publics_et_thematiques_240815_sample_of_1000.parquet -o 10p_accords_publics_et_thematiques_240815_sample_of_1000.parquet
